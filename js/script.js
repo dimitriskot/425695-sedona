e903a6d40acf90c9959dfb87593fee0b0ae98068
dimitriskot <30633226+dimitriskot@users.noreply.github.com>
2017-09-12 22:13:44 +0300

    Исправления для финальной проврки
    В файле index.html в конце разметки указаны комментарии к замечаниям.

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ var form = modalBox.querySelector("form");
 var arrivalDate = modalBox.querySelector("[name=arrival]");
 var departureDate = modalBox.querySelector("[name=departure]");
 
+modalBox.classList.add("modal-close");
 
 callModalBox.addEventListener("click", function (evt) {
 	evt.preventDefault();
@@ -14,7 +15,7 @@ form.addEventListener("submit", function (evt) {
 	if (!arrivalDate.value || !departureDate.value) {
 		evt.preventDefault();
 		modalBox.classList.remove("modal-error");
-     	modalBox.offsetWidth = modalBox.offsetWidth;
+		modalBox.offsetWidth = modalBox.offsetWidth;
 		modalBox.classList.add("modal-error");
 	}
 })
